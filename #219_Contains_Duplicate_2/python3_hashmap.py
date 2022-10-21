396df7f943f70a2e6bef18243e94d861930a2b6f
Mauro Leonelli <mleonelli@gmail.com>
2022-10-21 12:34:09 +0200

    Update python3_hashmap.py

--- #219_Contains_Duplicate_2/python3_hashmap.py
+++ #219_Contains_Duplicate_2/python3_hashmap.py
@@ -1,8 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        #This problem is extremely similar in solution to #1 Two Sum
+        #You forward pass once the list in O(n) time, validate if the requirement is met and if not
+        #the item is saved in hashmap for access in O(1) time
+        
         hashmap = {}
         for i, num in enumerate(nums):
             if num in hashmap and i - hashmap[num] <= k:
                 return True
             hashmap[num] = i
         return False
+    
+        #https://leetcode.com/submissions/detail/827226308/
+        #To evaluate the set approach as better in memory consumption
